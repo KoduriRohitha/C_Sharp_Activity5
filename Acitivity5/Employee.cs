a63d3ec05d9b13f4bfe7229fd09e8a783d691a1e
Rohitha Koduri <rohithakoduri@gmail.com>
2021-07-12 18:06:55 +0530

    activity5

---
+++ Acitivity5/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acitivity5BL
+{
+    public class Employee
+    {
+        public double basicSalary;
+        public double bonus;
+        public int employeeId;
+        public double BasicSalary
+        {
+            get
+            {
+                return basicSalary;
+            }
+            set
+            {
+                basicSalary = value;
+            }
+        }
+        public double Bonus
+        {
+            get
+            {
+                return bonus;
+            }
+            set
+            {
+                bonus = value;
+            }
+        }
+        public int EmployeeId
+        {
+            get
+            {
+                return employeeId;
+            }
+            set
+            {
+                employeeId = value;
+            }
+        }
+        public Employee()
+        {
+            BasicSalary = 10000;
+        }
+        public virtual double CalculateSalary()
+        {
+            return BasicSalary;
+        }
+    }
+}
---
+++ Activity5UI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActivityBL;
+
+namespace Activity5UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employeeObj = new SystemsEngineer("C#");
+            Finance financeObj = new Finance();
+            financeObj.GetCalculatedSalary(employeeObj);
+            Employee managerObj = new Manager();
+            financeObj.GetCalculatedSalary(managerObj);
+            Employee seniorprojectmanagerObj = new SeniorProjectManager();
+            financeObj.GetCalculatedSalary(seniorprojectmanagerObj);
+
+        }
+    }
+}
